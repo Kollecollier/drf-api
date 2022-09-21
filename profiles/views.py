3c9f532321243a4fa0fc3480e10db0400ec5ddab
Kollecollier <kristoffer.collier@live.se>
2022-09-21 14:31:05 +0000

    edited serializer py and imported

--- profiles/views.py
+++ profiles/views.py
@@ -1,37 +1,39 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
-from .serializer import ProfileSerializer
-from django.http import Http404
-from rest_framework import status
+from .serializers import ProfileSerializer
+
 
 
 class ProfileList(APIView):
     def get(self, request):
-        Profiles=profile.objects.all()
-        serializer=ProfileSerializer(Profiles, many=True)
-        return Response(serializer.data)
+        profiles = Profile.objects.all()
+        return Response(profiles)
+
+
 
 class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
-    def get_object(self,pk):
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
         try:
-            profile=Profile.objects.get(pk=pk)
+            profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
-        except Profile.DoesNotExist():
-            raise Http404()
+        except Profile.DoesNotExist:
+            raise Http404
 
     def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer=ProfileSerializer(profile)
+        serializer=ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
 
-    def put (self, request, pk):
-        profile =self.get_object(pk)
-        serializer=ProfileSerializer(profile,
-        data=request.data)
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors,
-        status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
