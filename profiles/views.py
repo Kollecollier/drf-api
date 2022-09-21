4789f6817562ca9826eedc46eb8afa863681f8fa
Kollecollier <kristoffer.collier@live.se>
2022-09-21 15:59:48 +0000

    made changes

--- profiles/views.py
+++ profiles/views.py
@@ -2,10 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from django.http import Http404
+from rest_framework import status
 
 
 
 class ProfileList(APIView):
+    serializer_class = ProfileSerializer
     def get(self, request):
         profiles = Profile.objects.all()
         return Response(profiles)
@@ -19,20 +22,18 @@ class ProfileDetail(APIView):
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
-        serializer=ProfileSerializer(profiles, many=True)
+        profile=self.get_object(pk)
+        serializer=ProfileSerializer(profile)
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request}
-        )
+        serializer = ProfileSerializer(profile, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
