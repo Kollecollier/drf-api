ccf7aacada1c7339c31ea7011d906ce61bb83041
Kollecollier <kristoffer.collier@live.se>
2022-09-21 06:24:24 +0000

    created serializer file and profileserializer, imported and added to profile list view

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializer import ProfileSerializer
 
 
 class ProfileList(APIView):
     def get(self, request):
         Profiles=profile.objects.all()
-        return Response(profiles)
+        serializer=ProfileSerializer(Profiles, many=True)
+        return Response(serializer.data)
