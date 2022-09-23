4a06307a3cd686fc27b90b18f6dd05ae8f4a3e90
Kollecollier <kristoffer.collier@live.se>
2022-09-23 12:36:18 +0000

    refactored all apps

--- likes/serializers.py
+++ likes/serializers.py
@@ -8,13 +8,12 @@ class LikesSerializer(serializers.ModelSerializer):
     
 class Meta:
     model = Like
-    fields = [
-        'id', 'created_at', 'owner', 'post'
-    ]
+    fields = ['id', 'created_at', 'owner', 'post']
 
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
-            raise serializers.ValidationError({'detail': 'possible duplicate'
+	            raise serializers.ValidationError({	
+                'detail': 'possible duplicate'	
             })
--- likes/views.py
+++ likes/views.py
@@ -11,3 +11,9 @@ class LikeList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permissions_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Like.objects.all()
