a6facb7d141dc1d6ed4db86214d3fe5b54536646
Kollecollier <kristoffer.collier@live.se>
2022-09-24 10:30:22 +0000

    done stuff

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from posts.models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
+    like_id = serializers.SerializerMethodField()
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -25,10 +27,20 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner = user, post = obj
+            ).first()
+            return like.id if like else None
+        return None
+
+
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id',
+            'id', 'owner', 'is_owner', 'like_id', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter'
         ]
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,10 +4,14 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
+
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image'
+            'posts_count', 'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image', 'followers_count', 'following_count'
         ]
